c72b9d2055538373a5835f9b5c854007e98d8580
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-04-11 17:14:59 -0400

    frontend wallet connect complete

---
+++ front_end/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ChainId, DAppProvider } from '@usedapp/core';
+import { Header } from './components/layout/Header';
+
+function App() {
+  return (
+    <DAppProvider
+      config={{
+        supportedChains: [ChainId.Kovan],
+        notifications: {
+          expirationPeriod: 1000,
+          checkInterval: 1000,
+        },
+      }}
+    >
+      <Header />
+    </DAppProvider>
+  );
+}
+
+export default App;
